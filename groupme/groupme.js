7141b2df2c4f9899032291a0cc46d9de67d66767
Noah Yoshida <nyoshida@nd.edu>
2019-02-06 00:00:33 -0800

    roasts

--- groupme/groupme.js
+++ groupme/groupme.js
@@ -13,11 +13,18 @@ const getWeather = require('./weather').getWeather
 // Message posting func
 var postMessage = require('./post_message').postMessage
 
-var roasts = ["Do you even have airpods?", "I thought I heard something? No? Jack?", "sudo kill -9 you"];
 var quotes = ["I̬̮̕ ̜A̧M͓ͅ ̷S̼̺͓ͅKY͕͎N̨̬̤͕E͕̞̳͈̻Ṯ̬̹", "h̵̠̀è̸̯͝l̷̹̓̍ĺ̷̲͍͗o̴̰̅ ̷̰͔̒̚w̵͈̿̋o̴̠͐r̷͈̫̔̾l̶̖̈̑d̶͕̩́"]; 
 
-
-var bobbits = ["I hear Bobbit hearts his own posts", "PHP? ...really?"];
+var roasts = {
+  general: ["Do you even have airpods?", 
+    "I thought I heard something? No? Jack?", 
+    "sudo kill -9 you"],
+  bobbit: ["I hear Bobbit hearts his own posts", 
+    "PHP? ...really?", 
+    "Black and white nano is the best editor"],
+  cfoley: ["Chris uses nano"],
+  jmeyer: ["Black and white nano is the best editor"]
+}
 
 var getRand = (roasts) => {
   
@@ -42,7 +49,9 @@ app.post('/', function (req, res) {
   console.log(req.body.text);
   let msg = req.body.text;
   if (msg.startsWith("!")) {
-    let cmd = msg.slice(1);
+    let sliced = msg.slice(1);
+    let cmds = slices.split(" ");
+    let cmd = cmds[0];
     console.log(cmd);
     switch(cmd) {
       case "weather": 
@@ -50,13 +59,22 @@ app.post('/', function (req, res) {
         getWeather(94301, 'us');
         break;
       case "roast":
-        postMessage(getRand(roasts));
+        // Roast these fools
+        let roastType = "general";
+        if (cmds.length > 1) 
+          roastType = cmds[1];
+
+        postMessage(getRand(roasts[roastType]));
         break;
       case "random":
         postMessage(getRand(quotes));
         break;
       case "bobbit":
         postMessage(getRand(bobbits));
+        break;
+      case "cfoley":
+        postMessage(getRand(chris));
+        break;
       default: 
         console.log("Invalid command!");
     }
